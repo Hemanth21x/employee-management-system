f4a02486676a94332a78b6d7f5edd8df1ebcaaff
Hemanth21x <hemanthkumar5980@gmail.com>
2026-09-25 18:14:58 +0530

    Allow deployed frontend CORS

--- backend/employee-management/src/main/java/com/example/employeemanagement/controller/EmployeeController.java
+++ backend/employee-management/src/main/java/com/example/employeemanagement/controller/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://employee-management-frontend-pa8b.onrender.com")
 @RestController
 @RequestMapping("api/v1")
 public class EmployeeController {
